42f91b82d4a078c129ff8df5fd2ca034b4978eca
sdy <3462325400@qq.com>
2025-09-05 18:17:06 +0800

    init:初始化父子工程学习仓库，并编写第一个接口

---
+++ spring-boot-quickstart/src/main/java/top/sdy/week1/controller/MoodController.java
@@ -0,0 +1,23 @@
+package top.sdy.week1.controller;
+
+import jdk.jfr.Registered;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Registered
+public class MoodController {
+    @Value("${my.mood.today}")
+    private String today;
+
+    @Value("${my.mood.content}")
+    private String content;
+
+    @Value("${my.mood.author}")
+    private String author;
+
+    @GetMapping("/mood")
+    public String getMood() {
+        return "今天是:" + today + ", 心情是:" + content+ ", 作者是:" + author;
+    }
+
+}
